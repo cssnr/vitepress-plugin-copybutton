8d02a3fb218604efa2228f47a5ffa189b43ac1da
Shane <6071159+smashedr@users.noreply.github.com>
2025-09-16 12:40:19 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+const name = 'CopyButton'
+
 export default defineConfig({
     plugins: [vue()],
     build: {
         lib: {
-            entry: path.resolve(__dirname, 'src/CopyButton.vue'),
-            name: 'CopyButton',
-            fileName: (format) => `CopyButton.${format}.js`,
+            entry: path.resolve(__dirname, `src/${name}.vue`),
+            name,
+            fileName: (format) => `${name}.${format}.js`,
         },
         cssCodeSplit: true,
         rollupOptions: {
