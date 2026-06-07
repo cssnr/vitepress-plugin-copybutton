c86462cd47785801ff4de605c170dd2e70d02c68
Shane <6071159+smashedr@users.noreply.github.com>
2026-06-07 16:59:30 -0700

    Add vitepress-chat

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,19 +2,25 @@ import { defineConfig } from 'eslint/config'
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
 
+// noinspection JSCheckFunctionSignatures,JSUnresolvedReference
 export default defineConfig([
   js.configs.recommended,
   tseslint.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   {
     rules: {
-      'no-undef': 'off',
+      // '@typescript-eslint/no-explicit-any': 'off',
+      // 'no-undef': 'off',
     },
     languageOptions: {
       sourceType: 'module',
       parserOptions: {
         parser: tseslint.parser,
+      },
+      globals: {
+        ...globals.browser,
       },
     },
   },
